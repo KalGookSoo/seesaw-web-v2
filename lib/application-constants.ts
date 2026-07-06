32b2864c287b16411e8bacce5970f6ed51e55cf7
Doyevskyi <ga.miro3721@gmail.com>
2026-07-06 17:05:20 +0900

    feat: 환경변수 동적으로 변경

--- lib/application-constants.ts
+++ lib/application-constants.ts
@@ -2,4 +2,4 @@ export const APPLICATION_NAME = process.env.SEESAW_APPLICATION_NAME ?? 'seesaw-w
 
 export const APPLICATION_VERSION = process.env.SEESAW_APPLICATION_VERSION ?? '0.0.1-SNAPSHOT';
 
-export const APPLICATION_API_BASE_URL = 'http://localhost:9000/seesaw-api/api';
+export const APPLICATION_API_BASE_URL = process.env.SEESAW_APPLICATION_API_BASE_URL ?? 'http://localhost:9000/seesaw-api/api';
