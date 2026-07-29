ca86c9df07d1ac450c465677957747352b94911f
Doyevskyi <ga.miro3721@gmail.com>
2026-07-29 12:26:37 +0900

    chore: 환경 변수 키를 NEXT_PUBLIC 접두사로 변경

--- lib/application-constants.ts
+++ lib/application-constants.ts
@@ -1,5 +1,9 @@
-export const APPLICATION_NAME = process.env.SEESAW_APPLICATION_NAME ?? 'seesaw-web-v2';
+export const APPLICATION_NAME =
+  process.env.NEXT_PUBLIC_SEESAW_APPLICATION_NAME ?? 'seesaw-web-v2';
 
-export const APPLICATION_VERSION = process.env.SEESAW_APPLICATION_VERSION ?? '0.0.1-SNAPSHOT';
+export const APPLICATION_VERSION =
+  process.env.NEXT_PUBLIC_SEESAW_APPLICATION_VERSION ?? '0.0.1-SNAPSHOT';
 
-export const APPLICATION_API_BASE_URL = process.env.SEESAW_APPLICATION_API_BASE_URL ?? 'http://localhost:9000/seesaw-api/api';
+export const APPLICATION_API_BASE_URL =
+  process.env.NEXT_PUBLIC_SEESAW_APPLICATION_API_BASE_URL ??
+  'http://localhost:9000/seesaw-api/api';
